a4644827f5ac661415b8f3341b547b0819ba4f03
OzbejOfficial <ozbejpavc@gmail.com>
2026-01-10 13:04:26 +0100

    ADD: Updated mediaDto to be the same as the one in Trip Service

--- src/main/kotlin/traversium/socialservice/dto/MediaDto.kt
+++ src/main/kotlin/traversium/socialservice/dto/MediaDto.kt
@@ -1,6 +1,9 @@
 package traversium.socialservice.dto
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
 import java.time.OffsetDateTime
 
 data class MediaDto(
@@ -10,9 +13,25 @@ data class MediaDto(
     val fileType: String? = null,
     val fileFormat: String? = null,
     val fileSize: Long? = null,
-    val geoLocation: String? = null,
+    val geoLocation: GeoLocation? = null,
     val createdAt : OffsetDateTime? = null,
 
     @get:JsonProperty(access = JsonProperty.Access.READ_ONLY)
     val uploadedAt: OffsetDateTime? = null,
 )
+
+@Embeddable
+data class GeoLocation(
+    @Column(name = "latitude")
+    val latitude: Double = 0.0,
+    @Column(name = "longitude")
+    val longitude: Double = 0.0
+){
+    companion object {
+        val UNKNOWN = GeoLocation()
+    }
+
+    @JsonIgnore
+    fun hasUnknownCoordinates(): Boolean =
+        latitude == 0.0 && longitude == 0.0
+}
